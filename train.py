05eb98cce7cf682fe970a372126ac5d0710ae518
Sam Grouchnikov <112674910+sam-hoodie@users.noreply.github.com>
2025-10-30 00:03:29 -0400

    training

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ def main():
     wandb_logger = WandbLogger(project="sudoku-testing", name="rb-b-512")
 
 
-    dataset = SudokuDataset("\home\sam\sudoku\labeled_sudoku.csv")
+    dataset = SudokuDataset("/home/sam/sudoku/labeled_sudoku.csv")
 
     train_size = int(0.7 * len(dataset))
     val_size = int(0.1 * len(dataset))
