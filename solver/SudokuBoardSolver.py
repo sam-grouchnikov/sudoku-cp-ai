abf0650068fb4043179d7165d33f1cd4e57ba212
Sam Grouchnikov <112674910+sam-hoodie@users.noreply.github.com>
2025-11-01 19:52:46 -0400

    rewrote solver

---
+++ solver/SudokuBoardSolver.py
@@ -0,0 +1,219 @@
+import pandas as pd
+import numpy as np
+import torch
+import copy
+
+from sympy.polys import domains
+
+from CNNGuider.model import SudokuLightning
+
+
+def get_possible_vals_from_domain(domain_vec):
+    return [i + 1 for i, v in enumerate(domain_vec) if v == 1]
+
+
+def find_first_empty(board):
+    for r in range(9):
+        for c in range(9):
+            if board[r][c] == 0:
+                return [r, c]
+    return None
+
+
+class SudokuBoard:
+    def __init__(self, path):
+        self.path = path
+        self.boardDF = pd.read_csv(path)
+        self.board = self.boardDF.fillna(0).astype(int).values.tolist()
+        self.domainStore = [[[1 for _ in range(9)] for _ in range(9)] for _ in range(9)]
+        self.initializeDomains()
+
+        ckpt_path = "C:\\Users\\samgr\\PycharmProjects\\sudoku-cp-ai\\solver\\row_ckpt.ckpt"
+        self.model = SudokuLightning.load_from_checkpoint(ckpt_path)
+
+    def predict(self, board):
+        self.model.eval()
+        board_array = torch.tensor(board, dtype=torch.float32).view(1, 1, 9, 9)
+
+        with torch.no_grad():
+            logits = self.model(board_array)
+            preds = logits.view(-1).argmax()
+
+        row, col = divmod(preds.item(), 9)
+        return [row, col]
+
+    def initializeDomains(self):
+        for r in range(9):
+            for c in range(9):
+                val = self.board[r][c]
+                if val != 0:
+                    for d in range(9):
+                        self.domainStore[r][c][d] = 0
+                    self.domainStore[r][c][val - 1] = 1
+            self.propagateRows(r)
+            self.propagateCols(r)
+            self.propagateGrids(r)
+
+    def propagate(self):
+        for r in range(9):
+            self.propagateRows(r)
+            self.propagateCols(r)
+            self.propagateGrids(r)
+
+    def propagateRows(self, r):
+        filledVals = [self.board[r][c] for c in range(9) if self.board[r][c] != 0 ]
+
+        for c in range(9):
+            cellVal = self.board[r][c]
+            if cellVal != 0:
+                for i in range(9):
+                    self.domainStore[r][c][i] = 1 if i == cellVal - 1 else 0
+
+            else:
+                for i in range(9):
+                    if (i + 1) in filledVals:
+                        self.domainStore[r][c][i] = 0
+
+    def propagateCols(self, colIdx):
+        filledVals = [self.board[r][colIdx] for r in range(9) if self.board[r][colIdx] != 0]
+
+        for rowIdx in range(9):
+            cellVal = self.board[rowIdx][colIdx]
+            if cellVal != 0:
+                for i in range(9):
+                    self.domainStore[rowIdx][colIdx][i] = 1 if i == cellVal - 1 else 0
+            else:
+                for i in range(9):
+                    if (i + 1) in filledVals:
+                        self.domainStore[rowIdx][colIdx][i] = 0
+
+    def propagateGrids(self, gridIdx):
+        startRow = (gridIdx // 3) * 3
+        startCol = (gridIdx % 3) * 3
+
+        filledVals = []
+        for r in range(startRow, startRow + 3):
+            for c in range(startCol, startCol + 3):
+                val = self.board[r][c]
+                if val != 0:
+                    filledVals.append(val)
+
+        for r in range(startRow, startRow + 3):
+            for c in range(startCol, startCol + 3):
+                cellVal = self.board[r][c]
+                if cellVal != 0:
+                    for i in range(9):
+                        self.domainStore[r][c][i] = 1 if i == cellVal - 1 else 0
+                else:
+                    for i in range(9):
+                        if (i + 1) in filledVals:
+                            self.domainStore[r][c][i] = 0
+
+    def apply_assignment_and_propagate(self, board, domainStore, r, c, value):
+        board = copy.deepcopy(board)
+        domainStore = copy.deepcopy(domainStore)
+
+        board[r][c] = value
+
+        for d in range(9):
+            domainStore[r][c][d] = 1 if d == (value - 1) else 0
+
+        queue = [(r, c)]
+
+        while queue:
+            cr, cc = queue.pop
+            assigned_vec = domains[cr][cc]
+            try:
+                assigned_val = assigned_vec.index(1) + 1
+            except ValueError:
+                return None, None
+
+            for col in range(9):
+                if col == cc:
+                    continue
+                if board[cr][col] == 0 and domainStore[cr][col][assigned_val - 1] == 1:
+                    domainStore[cr][col][assigned_val - 1] = 0
+                    if sum(domainStore[cr][col]) == 0:
+                        return None, None
+                    if sum(domainStore[cr][col]) == 1:
+                        new_val = domainStore[cr][col].index(1) + 1
+                        board[cr][col] = new_val
+                        queue.append((cr, col))
+
+                # Column propagation
+            for row in range(9):
+                if row == cr:
+                    continue
+                if board[row][cc] == 0 and domainStore[row][cc][assigned_val - 1] == 1:
+                    domainStore[row][cc][assigned_val - 1] = 0
+                    if sum(domainStore[row][cc]) == 0:
+                        return None, None
+                    if sum(domainStore[row][cc]) == 1:
+                        new_val = domainStore[row][cc].index(1) + 1
+                        board[row][cc] = new_val
+                        queue.append((row, cc))
+
+                # Grid propagation
+            startRow = (cr // 3) * 3
+            startCol = (cc // 3) * 3
+            for rr in range(startRow, startRow + 3):
+                for cc2 in range(startCol, startCol + 3):
+                    if rr == cr and cc2 == cc:
+                        continue
+                    if board[rr][cc2] == 0 and domainStore[rr][cc2][assigned_val - 1] == 1:
+                        domainStore[rr][cc2][assigned_val - 1] = 0
+                        if sum(domainStore[rr][cc2]) == 0:
+                            return None, None
+                        if sum(domainStore[rr][cc2]) == 1:
+                            new_val = domainStore[rr][cc2].index(1) + 1
+                            board[rr][cc2] = new_val
+                            queue.append((rr, cc2))
+
+        return board, domainStore
+
+    def search(self, board=None, domainStore=None):
+        if board is None:
+            board = copy.deepcopy(self.board)
+        if domainStore is None:
+            domainStore = copy.deepcopy(self.domainStore)
+
+        if all(all(val != 0 for val in row) for row in board):
+            return board
+
+        try:
+            nr, nc = self.predict(board)
+        except:
+            nr, nc = find_first_empty(board)
+
+        if board[nr][nc] != 0:
+            nr, nc = find_first_empty(board)
+
+        cell_domain = domainStore[nr][nc]
+        possible_values = get_possible_vals_from_domain(cell_domain)
+
+        for val in possible_values:
+            new_board, new_domain = self.apply_assignment_and_propagate(board, domainStore, nr, nc, val)
+            if new_board is None:
+                continue
+
+            result = self.search(new_board, new_domain)
+            if result is not None:
+                return result
+
+        return None
+
+    def print(self):
+        print("Board:")
+        print("    " + " ".join(f"C{c}" for c in range(9)))
+        for r in range(9):
+            row_vals = " ".join(str(self.board[r][c]) for c in range(9))
+            print(f"R{r}  {row_vals}")
+
+        print("\nDomain Store:")
+        for r in range(9):
+            print(f"\nRow R{r}:")
+            for c in range(9):
+                domain_vec = self.domainStore[r][c]
+                possible_vals = [str(i + 1) for i in range(9) if domain_vec[i] == 1]
+                possible_str = "{" + ",".join(possible_vals) + "}" if possible_vals else "-"
+                print(f"  Cell (R{r},C{c}): {domain_vec}  ->  {possible_str}")
