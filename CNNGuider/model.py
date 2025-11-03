541a3b7a421913aeea1890ead23b02a0553f91e5
Sam Grouchnikov <112674910+sam-hoodie@users.noreply.github.com>
2025-11-03 16:18:37 -0500

    larger model

--- CNNGuider/model.py
+++ CNNGuider/model.py
@@ -7,27 +7,33 @@ class SudokuCNN(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(in_channels=30, out_channels=128, kernel_size=3, padding=1)
-        # self.conv2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1)
         self.conv_out = nn.Conv2d(in_channels=128, out_channels=64, kernel_size=1)
 
         self.fc1 = nn.Linear(64 * 9 * 9, 512)
-        # self.fc2 = nn.Linear(512, 512)
-        self.fc3 = nn.Linear(512, 9 * 9)
+        self.fc2 = nn.Linear(512, 512)
+        self.fc3 = nn.Linear(512, 512)
+        self.fc4 = nn.Linear(512, 9 * 9)
 
         self.bn1 = nn.BatchNorm2d(128)
         self.bn2 = nn.BatchNorm2d(128)
+        self.bn3 = nn.BatchNorm2d(128)
 
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.dropout(x, p=0.2, training=self.training)
-        # x = F.relu(self.bn2(self.conv2(x)))
-        # x = F.dropout(x, p=0.2, training=self.training)
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.dropout(x, p=0.2, training=self.training)
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.dropout(x, p=0.2, training=self.training)
         x = self.conv_out(x)
         x = x.view(x.size(0), -1)
 
         x = F.relu(self.fc1(x))
-        # x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
         x = x.view(-1, 9, 9)
         return x
 
