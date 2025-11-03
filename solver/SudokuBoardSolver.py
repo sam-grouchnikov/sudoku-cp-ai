9c39dd3ca0e6a4625c3f4b25bc0bdc45ff34a551
Sam Grouchnikov <112674910+sam-hoodie@users.noreply.github.com>
2025-11-02 19:22:07 -0500

    debugging

--- solver/SudokuBoardSolver.py
+++ solver/SudokuBoardSolver.py
@@ -88,7 +88,7 @@ def isSolved(board):
 
 class SudokuBoard:
     def __init__(self, board_string):
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.board_string = board_string
         flat_array = np.array([int(char) for char in board_string])
         self.board = flat_array.reshape(9, 9).tolist()
@@ -96,13 +96,13 @@ class SudokuBoard:
         self.initializeDomains()
         self.recursiveCalls = 0
 
-        ckpt_path = "C:\\Users\\samgr\\PycharmProjects\\sudoku-cp-ai\\solver\\row_ckpt.ckpt"
+        # ckpt_path = "C:\\Users\\samgr\\PycharmProjects\\sudoku-cp-ai\\solver\\row_ckpt.ckpt"
         ckpt_path = "/home/sam/sudoku/sudoku-cp-ai/row_ckpt.ckpt"
-        self.model = SudokuLightning.load_from_checkpoint(ckpt_path).to(device)
+        self.model = SudokuLightning.load_from_checkpoint(ckpt_path).to(self.device)
 
     def predict(self, board):
         self.model.eval()
-        board_array = torch.tensor(board, dtype=torch.float32).view(1, 1, 9, 9)
+        board_array = torch.tensor(board, dtype=torch.float32, device = self.device).view(1, 1, 9, 9)
 
         with torch.no_grad():
             logits = self.model(board_array)
