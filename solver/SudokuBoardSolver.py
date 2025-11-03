b234c755bea24671b9d9e361a4be704533877dc4
Sam Grouchnikov <112674910+sam-hoodie@users.noreply.github.com>
2025-11-03 11:28:11 -0500

    more channels

--- solver/SudokuBoardSolver.py
+++ solver/SudokuBoardSolver.py
@@ -96,8 +96,8 @@ class SudokuBoard:
         self.initializeDomains()
         self.recursiveCalls = 0
 
-        ckpt_path = "C:\\Users\\samgr\\PycharmProjects\\sudoku-cp-ai\\solver\\row_ckpt.ckpt"
-        # ckpt_path = "/home/sam/sudoku/sudoku-cp-ai/row_ckpt.ckpt"
+        # ckpt_path = "C:\\Users\\samgr\\PycharmProjects\\sudoku-cp-ai\\solver\\row_ckpt.ckpt"
+        ckpt_path = "/home/sam/sudoku/sudoku-cp-ai/row_ckpt.ckpt"
         self.model = SudokuLightning.load_from_checkpoint(ckpt_path).to(self.device)
 
     def predict(self, board):
