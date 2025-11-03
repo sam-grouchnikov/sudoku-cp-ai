e9cca14499bc4cec39b4d234b7f1cbaceef9a65b
Sam Grouchnikov <112674910+sam-hoodie@users.noreply.github.com>
2025-11-02 19:47:35 -0500

    debugging

---
+++ ModelTesting.py
@@ -0,0 +1,26 @@
+import torch
+import pandas as pd
+
+from solver.SudokuBoardSolver import SudokuBoard
+
+sudoku_file = "C:\\Users\\samgr\\PycharmProjects\\sudoku-cp-ai\\sudoku.csv"
+# sudoku_file = "/home/sam/sudoku/sudoku-3m.csv"
+df = pd.read_csv(sudoku_file, nrows=20000)
+boards = df.iloc[:, 0].astype(str).tolist()
+
+num_incorrect = 0
+
+# board_temp = boards[0]
+# sb = SudokuBoard(board_temp)
+# pred = sb.predict(sb.getBoard())
+# if sb.getValue(pred[0], pred[1]) != 0:
+#    sb.print()
+#    print(pred)
+
+for board in boards:
+    sb = SudokuBoard(board)
+    pred = sb.predict(sb.getBoard())
+    if sb.getValue(pred[0], pred[1]) != 0:
+        num_incorrect += 1
+
+print("Num incorrect:", num_incorrect)
