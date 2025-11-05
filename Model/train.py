7ca4b34371eb37a5c3991d78bf9c66f1a2fe2c32
Sam Grouchnikov <112674910+sam-hoodie@users.noreply.github.com>
2025-11-05 14:53:28 -0500

    smaller cnn

--- Model/train.py
+++ Model/train.py
@@ -4,6 +4,7 @@ from torch.utils.data import DataLoader, random_split
 import torch
 from lightning.pytorch.loggers import WandbLogger
 from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping
+from lightning.pytorch.callbacks import TQDMProgressBar
 
 
 from Model.Dataset import SudokuDataset
@@ -36,6 +37,7 @@ def main():
 
 
     trainer = pl.Trainer(
+        callbacks = [TQDMProgressBar],
         max_epochs=epochs,
         accelerator="gpu",
         devices=devices,
