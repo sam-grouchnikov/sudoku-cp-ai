19e45677f4c6933943a2783e341c7ced86d23a89
Sam Grouchnikov <112674910+sam-hoodie@users.noreply.github.com>
2025-11-07 18:34:26 -0500

    linear

--- Model/model_normal.py
+++ Model/model_normal.py
@@ -7,10 +7,10 @@ class SudokuFC(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.fc1 = nn.Linear(784, 784)
-        self.fc2 = nn.Linear(784, 784)
-        self.fc3 = nn.Linear(784, 784)
-        self.fc4 = nn.Linear(784, 9 * 9)
+        self.fc1 = nn.Linear(784, 1024)
+        self.fc2 = nn.Linear(1024, 1024)
+        self.fc3 = nn.Linear(1024, 1024)
+        self.fc4 = nn.Linear(1024, 9 * 9)
 
     def forward(self, x):
         x = x.view(x.size(0), -1)
