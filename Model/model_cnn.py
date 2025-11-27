63bd2cf6f1d861ea701bd4c3bc7c66204222208a
Sam Grouchnikov <112674910+sam-hoodie@users.noreply.github.com>
2025-11-27 10:11:06 -0500

    cnn8 rawfeat

--- Model/model_cnn.py
+++ Model/model_cnn.py
@@ -9,9 +9,9 @@ class SudokuCNN(nn.Module):
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1)
         self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1)
-        self.conv_out = nn.Conv2d(in_channels=64, out_channels=4, kernel_size=1)
+        self.conv_out = nn.Conv2d(in_channels=64, out_channels=8, kernel_size=1)
 
-        self.fc1 = nn.Linear(4 * 9 * 9, 256)
+        self.fc1 = nn.Linear(8 * 9 * 9, 256)
         self.fc2 = nn.Linear(256, 256)
         self.fc3 = nn.Linear(256, 256)
         self.fc4 = nn.Linear(256, 9 * 9)
