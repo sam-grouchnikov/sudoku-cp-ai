86378ebb76c5e260266d65d8cae40db8fe9e6c2c
Sam Grouchnikov <112674910+sam-hoodie@users.noreply.github.com>
2026-01-10 21:55:28 -0500

    up to date

--- Model/train.py
+++ Model/train.py
@@ -32,7 +32,7 @@ def main():
     val_loader = DataLoader(val_dataset, batch_size=1028, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=1028)
 
-    model = SudokuLightningFC(wandb_logger)
+    model = SudokuLightningCNN(wandb_logger)
 
 
 
